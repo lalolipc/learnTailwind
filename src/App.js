07f493835aa421ee5cc80b53e5179fe0fb8bacab
rgocella <rgocella@smartnet.com.ar>
2021-07-11 19:53:25 -0300

    modify one letter on http

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
         React
       </p>
     </div>
-    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" ><a href="http://aprenderingles.vercel.app">Enter</a></button>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" ><a href="https://aprenderingles.vercel.app">Enter</a></button>
     </div>
 
  
