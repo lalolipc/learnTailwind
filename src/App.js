a67c461d1a88a21ff5483cb91b3b88cc043e78e0
rgocella <rgocella@smartnet.com.ar>
2021-07-11 03:37:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+
+
+
+      
+<div class="grid grid-cols-6 gap-4">
+
+
+
+
+{/*me*/}
+
+  <div class="col-start-1 col-end-7 xs:col-span-6 md:col-start-1 md:col-end-7  " >
+<div class=" max-w-md mx-auto  bg-white  shadow-md overflow-hidden md:max-w-2xl ">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class=" w-full object-cover md:h-full  md:w-48" src="img/rociogocellacanva.jpg" alt="Man looking at item at a store "  ></img>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Rocio Gocella</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Java Developer</a>
+      <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+          {/*3er section proyects*/}
+</div>
+<div className="col-start-1 col-end-7 xs:col-span-6 md:col-start-1 md:col-end-7 ">
+<div class="col-start-1 col-end-6" >
+<div class="mt-10 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 order-3">
+  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="img/js.png" alt="Woman's Face"></img>
+  <div class="text-center space-y-2 sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold">
+        Piedra, papel ó tijera
+      </p>
+      <p class="text-gray-500 font-medium">
+        Javascript 
+      </p>
+    </div>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"><a href="https://gamepiedrapapeltijera.000webhostapp.com">Enter</a></button>
+  </div>
+
+ 
+
+</div>
+</div>
+<div class="col-start-1 col-end-6" >
+  <div class="mt-10 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 order-3">
+  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="img/react.png" alt="Woman's Face"></img>
+   <div class="text-center space-y-2 sm:text-left">
+      <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold">
+        Frases en ingles para usuarios
+      </p>
+      <p class="text-gray-500 font-medium">
+        React
+      </p>
+    </div>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" ><a href="http://aprenderingles.vercel.app">Enter</a></button>
+    </div>
+
+ 
+
+  </div>
+</div>
+
+<div class="col-start-1 col-end-6" >
+  <div class="mt-10 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 order-3">
+  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="img/angular.png" alt="Woman's Face"></img>
+   <div class="text-center space-y-2 sm:text-left">
+      <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold">
+        Ingles niños
+      </p>
+      <p class="text-gray-500 font-medium">
+        Angular
+      </p>
+    </div>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" ><a href="https://bilingualchildren.herokuapp.com">Enter</a></button>
+    </div>
+
+ 
+
+  </div>
+</div>
+
+</div>
+</div>
+
+
+  );
+}
+
+export default App;
